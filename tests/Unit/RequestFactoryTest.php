f23b8ded600b020bec5bbab86e16f496acafb335
west <457395070@qq.com>
2026-04-19 17:17:50 +0800

    chore: migrate repository links to whalesky-labs

--- tests/Unit/RequestFactoryTest.php
+++ tests/Unit/RequestFactoryTest.php
@@ -5,10 +5,10 @@ declare(strict_types=1);
 /**
  * This file is part of KwaiShopSDK.
  *
- * @link     https://github.com/westng/kwaishop-php-sdk
- * @document https://github.com/westng/kwaishop-php-sdk
+ * @link     https://github.com/whalesky-labs/kwaishop-php-sdk
+ * @document https://github.com/whalesky-labs/kwaishop-php-sdk
  * @contact  westng
- * @license  https://github.com/westng/kwaishop-php-sdk/blob/main/LICENSE
+ * @license  https://github.com/whalesky-labs/kwaishop-php-sdk/blob/main/LICENSE
  */
 
 namespace KwaiShopSDK\Tests\Unit;
